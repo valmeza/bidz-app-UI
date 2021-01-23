6ab2bb43af990ebacfa8e9ad7f2828285310b5af
Jesus Rogelio Garza <jesusgarza2511@gmail.com>
2021-01-22 18:59:21 -0600

    Added in new components directory with item component, new models directory with item model, pages directory with item-details, and services directory to store the item services ts file. Used ng generate to build these directories (except for models/item.ts).

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ItemComponent } from './components/item/item.component';
+import { ItemDetailsComponent } from './pages/item-details/item-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ItemComponent,
+    ItemDetailsComponent
   ],
   imports: [
     BrowserModule,
